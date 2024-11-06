c3b91d9d8046758f1b7e60c863314eb7b0cd6e2f
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-11-06 19:11:30 +0530

    Initial Commit

---
+++ My_Contacts_App/My_Contacts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Contacts_AppApp.swift
+//  My_Contacts_App
+//
+//  Created by Thushini Abeysuriya on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct My_Contacts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
